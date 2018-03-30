4d021ba140bb7711a76047d3c496cf8c9f214e4e
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-03-30 02:15:38 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,7 +1,12 @@
 module Players
   class Computer < Player
     def move(board)
-      
+      result
+      board.cells.each_with_index do |x,i|
+        if x == ' '
+          result = i
+        end
+      end
 
 
     end
